c51e4838ff66db003677a317ba4e6f3b733fe803
Arsalan Babar <ababar3@uic.edu>
2019-10-25 02:16:09 -0500

    TV_E FULLY WORKING AND GENERATING TEST VECTORS
    TV_E FULLY WORKING AND GENERATING TEST VECTORS

--- TV_GEN.py
+++ TV_GEN.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import os
-
-def TV_A(circuitName):
+import math as m
+def TV_A(circuitName, seedInitial):
     netFile = open(circuitName, 'r')  #open the circuit bench and read that file
 
             #while True:
@@ -16,10 +16,10 @@ def TV_A(circuitName):
                  #   break
 
     outputFile = open(outputName, 'w')
-    outputFile.write("# TV SET A \n")
+    outputFile.write("# TV SET A \n" + "Initial Seed: " + seedInitial+"\n\n")
 
 
-def TV_B(circuitName):
+def TV_B(circuitName, seedInitial):
     netFile = open(circuitName, 'r')  #open the circuit bench and read that file
 
             #while True:
@@ -33,11 +33,11 @@ def TV_B(circuitName):
                  #   break
 
     outputFile = open(outputName, 'w')
-    outputFile.write("# TV SET B \n")
+    outputFile.write("# TV SET B \n"+ seedInitial+"\n\n")
 
 
 
-def TV_C(circuitName):
+def TV_C(circuitName, seedInitial):
     netFile = open(circuitName, 'r')  #open the circuit bench and read that file
 
             #while True:
@@ -51,9 +51,9 @@ def TV_C(circuitName):
                  #   break
 
     outputFile = open(outputName, 'w')
-    outputFile.write("# TV SET C \n")
+    outputFile.write("# TV SET C \n" + seedInitial+"\n\n")
 
-def TV_D(circuitName):
+def TV_D(circuitName, seedInitial):
     netFile = open(circuitName, 'r')  #open the circuit bench and read that file
 
             #while True:
@@ -67,13 +67,24 @@ def TV_D(circuitName):
                  #   break
 
     outputFile = open(outputName, 'w')
-    outputFile.write("# TV SET D \n")
+    outputFile.write("# TV SET D \n" + seedInitial+"\n\n")
 
-def TV_E(circuitName):
-        netFile = open(circuitName, 'r')  # open the circuit bench and read that file
+def TV_E(circuitName, seedInitial):
+    netFile = open(circuitName, 'r')  # open the circuit bench and read that file
+    outputName= "TV_E.txt"
+    outputFile = open(outputName, 'w')
+    outputFile.write("# TV SET E \n" + "Initial Seed: " + seedInitial + "\n\n")
+    n=0
+    j=1
 
+    for line in netFile:
+        if line[0:5]=="INPUT":
+            n=n+1
+    print("N Size from circuit: " + str(n))
+    loopTime= m.ceil(n/8)
+    print (str(loopTime))
         # while True:
-        outputName = "TV_E.txt"
+    #outputName = "TV_E.txt"
         # print("\n Write output file: use " + outputName + "?" + " Enter to accept or type filename: ")
         # userInput = input()
         # if userInput == "":
@@ -81,9 +92,133 @@ def TV_E(circuitName):
         # else:
         #   outputName = os.path.join(script_dir, userInput)
         #   break
+    reversedSeedInitial = ''.join(reversed(seedInitial))
+    lst=[]
+    outputList=[]
+    tvSet=[]
+    tv2Set=[]
+    tvSet.insert(0, reversedSeedInitial)
+    str1 = ""
+    p=""
+    i=1
 
-        outputFile = open(outputName, 'w')
-        outputFile.write("# TV SET E \n")
+    while i!= loopTime:
+        for bit in reversedSeedInitial:
+            lst.append(bit)
+
+        print("Reversed:" + str(lst))
+
+
+        Shiftedlst = [lst[-1]] + lst[:-1]
+        print("Shifted: " + str(Shiftedlst))
+        #for bit in lst:
+        if lst[len(seedInitial)-1]=='0' and Shiftedlst[2]=='0':
+            Shiftedlst[2]='0'
+
+        elif lst[len(seedInitial)-1]=='1' and Shiftedlst[2]=='0':
+            Shiftedlst[2]='1'
+
+        elif lst[len(seedInitial)-1] == '0' and Shiftedlst[2] == '1':
+            Shiftedlst[2]='1'
+
+        elif lst[len(seedInitial)-1] == '1' and Shiftedlst[2] == '1':
+            Shiftedlst[2]='0'
+
+
+        if lst[len(seedInitial)-1]=='0' and Shiftedlst[3]=='0':
+            Shiftedlst[3]='0'
+
+        elif lst[len(seedInitial)-1]=='1' and Shiftedlst[3]=='0':
+            Shiftedlst[3]='1'
+
+        elif lst[len(seedInitial)-1] == '0' and Shiftedlst[3] == '1':
+            Shiftedlst[3]='1'
+
+        elif lst[len(seedInitial)-1] == '1' and Shiftedlst[3] == '1':
+            Shiftedlst[3]='0'
+
+
+        if lst[len(seedInitial)-1] == '0' and Shiftedlst[4] == '0':
+            Shiftedlst[4]='0'
+        elif lst[len(seedInitial)-1] == '1' and Shiftedlst[4] == '0':
+            Shiftedlst[4]='1'
+
+        elif lst[len(seedInitial)-1] == '0' and Shiftedlst[4] == '1':
+            Shiftedlst[4]='1'
+
+        elif lst[len(seedInitial)-1] == '1' and Shiftedlst[4] == '1':
+            Shiftedlst[4] = '0'
+        str1= str1.join(Shiftedlst)
+        tvSet.insert(i, str1)
+        reversedSeedInitial=str1
+        lst.clear()
+        Shiftedlst.clear()
+        str1=""
+
+        i = i+1
+    p = p.join(tvSet)
+    p=p[0:n]
+    reversedP = ''.join(reversed(p))
+    outputFile.write(reversedP+"\n")
+    b=len(p)-1
+    while j != 255:
+        for bit in p:
+            lst.append(bit)
+        Shiftedlst = [lst[-1]] + lst[:-1]
+        if lst[b] == '0' and Shiftedlst[2] == '0':
+            Shiftedlst[2] = '0'
+
+        elif lst[b] == '1' and Shiftedlst[2] == '0':
+            Shiftedlst[2] = '1'
+
+        elif lst[b] == '0' and Shiftedlst[2] == '1':
+            Shiftedlst[2] = '1'
+
+        elif lst[b] == '1' and Shiftedlst[2] == '1':
+            Shiftedlst[2] = '0'
+
+        if lst[b] == '0' and Shiftedlst[3] == '0':
+            Shiftedlst[3] = '0'
+
+        elif lst[b] == '1' and Shiftedlst[3] == '0':
+            Shiftedlst[3] = '1'
+
+        elif lst[b] == '0' and Shiftedlst[3] == '1':
+            Shiftedlst[3] = '1'
+
+        elif lst[b] == '1' and Shiftedlst[3] == '1':
+            Shiftedlst[3] = '0'
+
+        if lst[b] == '0' and Shiftedlst[4] == '0':
+            Shiftedlst[4] = '0'
+        elif lst[b] == '1' and Shiftedlst[4] == '0':
+            Shiftedlst[4] = '1'
+
+        elif lst[b] == '0' and Shiftedlst[4] == '1':
+            Shiftedlst[4] = '1'
+
+        elif lst[b] == '1' and Shiftedlst[4] == '1':
+            Shiftedlst[4] = '0'
+        str1 = str1.join(Shiftedlst)
+        toGo=''.join(reversed(str1))
+        outputFile.write(toGo+"\n")
+        p=str1
+        str1=""
+        lst.clear()
+        Shiftedlst.clear()
+        j=j+1
+
+
+    print("Normal: " + p)
+
+    print("REverse: " + reversedP)
+   # print(reversedSeedInitial)
+    print("AFTER OR:" + str(Shiftedlst))
+    print("TEST VECTOR SET: " + str(tvSet))
+
+
+
+
 
 
 def main():
@@ -101,11 +236,34 @@ def main():
                 print("File does not exist. \n")
             else:
                 break
-    TV_A(cktFile)
-    TV_B(cktFile)
-    TV_C(cktFile)
-    TV_D(cktFile)
-    TV_E(cktFile)
+
+    print("Please enter the initial seed for " + cktFile + ": ")
+
+    while True:
+
+        userSeed= input()
+
+        if userSeed == "":
+            print("Please enter the initial seed for " + cktFile + " (8 bits): ")
+
+        #elif len(userSeed)!=8:
+            #print("Please enter the initial seed for " + cktFile + " (8 bits): ")
+
+
+
+        else:
+            seedInitial=userSeed
+            print("Initial Seed: "+seedInitial)
+            break
+
+
+
+
+    TV_A(cktFile, seedInitial)
+    TV_B(cktFile, seedInitial)
+    TV_C(cktFile, seedInitial)
+    TV_D(cktFile, seedInitial)
+    TV_E(cktFile, seedInitial)
 
 if __name__ == "__main__":
     main()
