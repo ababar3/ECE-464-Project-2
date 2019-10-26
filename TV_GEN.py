19c9c2ab4266aa5f455cf10303d27179027f9ec9
Arsalan Babar <ababar3@uic.edu>
2019-10-25 21:13:44 -0500

    update part TV-a working
    TV_ b in progress
    
    TV_E one bit off(debugging)

--- TV_GEN.py
+++ TV_GEN.py
@@ -1,13 +1,71 @@
 from __future__ import print_function
+from functools import reduce
 import os
 import math as m
+
+def sublist(lst, n):
+    sub=[] ; result=[]
+    for i in lst:
+        sub+=[i]
+        if len(sub)==n: result+=[sub] ; sub=[]
+    if sub: result+=[sub]
+    return result
+
 def TV_A(circuitName, seedInitial):
+    lst=[]
+    sum=""
+    x=""
+    str1=""
     netFile = open(circuitName, 'r')  #open the circuit bench and read that file
 
             #while True:
     outputName = "TV_A.txt"
+    outputFile = open(outputName, 'w')
+    outputFile.write("# TV SET A \n" + "Initial Seed: " + seedInitial + "\n\n")
+    n=0
 
-                #print("\n Write output file: use " + outputName + "?" + " Enter to accept or type filename: ")
+    for line in netFile:
+        if line[0:5] == "INPUT":
+            n = n + 1
+    #loopTime = m.ceil(n / 8)
+    i=0
+    k=n-8
+    while k!=0:
+        lst.append('0')
+        k=k-1
+
+
+    str1 = str1.join(lst)
+
+
+    reversedSeedInitial = ''.join(reversed(seedInitial))
+    joined= str1+seedInitial
+
+    outputFile.write(joined+"\n")
+
+    num1 = '00001'
+    # decimal value 17
+    num2 = '10001'
+
+    # sum - decimal value 18
+    # binary value 10010
+    i=0
+    while i<255:
+        num1= seedInitial
+        num2='0001'
+        sum = bin(int(num1, 2) + int(num2, 2))
+        sum=sum.replace('0b', '')
+       # if len(sum)==9:
+            #x=str1[1:]
+        x=str1+sum
+        if len(x)>n:
+            x =x[1:]
+        outputFile.write(x+"\n")
+        seedInitial=sum
+        x=""
+        i=i+1
+
+            #print("\n Write output file: use " + outputName + "?" + " Enter to accept or type filename: ")
                 #userInput = input()
                 #if userInput == "":
                  #   break
@@ -15,25 +73,62 @@ def TV_A(circuitName, seedInitial):
                  #   outputName = os.path.join(script_dir, userInput)
                  #   break
 
-    outputFile = open(outputName, 'w')
-    outputFile.write("# TV SET A \n" + "Initial Seed: " + seedInitial+"\n\n")
+
+
 
 
 def TV_B(circuitName, seedInitial):
-    netFile = open(circuitName, 'r')  #open the circuit bench and read that file
+    lst = []
+    sum = ""
+    x = ""
+    str1 = ""
+    netFile = open(circuitName, 'r')  # open the circuit bench and read that file
 
-            #while True:
+    # while True:
     outputName = "TV_B.txt"
-                #print("\n Write output file: use " + outputName + "?" + " Enter to accept or type filename: ")
-                #userInput = input()
-                #if userInput == "":
-                 #   break
-                #else:
-                 #   outputName = os.path.join(script_dir, userInput)
-                 #   break
-
     outputFile = open(outputName, 'w')
-    outputFile.write("# TV SET B \n"+ seedInitial+"\n\n")
+    outputFile.write("# TV SET B \n" + "Initial Seed: " + seedInitial + "\n\n")
+    n = 0
+
+    for line in netFile:
+        if line[0:5] == "INPUT":
+            n = n + 1
+    # loopTime = m.ceil(n / 8)
+
+
+    #str1 = str1.join(lst)
+
+
+
+    #joined = str1 + seedInitial
+    i=0
+    loopTime = m.ceil(n / 8)
+    while i<loopTime:
+        lst.append(seedInitial)
+        i=i+1
+    str1 = str1.join(lst)
+
+    outputFile.write(str1)
+    exit()
+    # sum - decimal value 18
+    # binary value 10010
+    i = 0
+    while i < 255:
+        num1 = seedInitial
+        num2 = '0001'
+        sum = bin(int(num1, 2) + int(num2, 2))
+        sum = sum.replace('0b', '')
+        # if len(sum)==9:
+        # x=str1[1:]
+        x = str1 + sum
+        if len(x) > n:
+            x = x[1:]
+        outputFile.write(x + "\n")
+        seedInitial = sum
+        x = ""
+        i = i + 1
+
+
 
 
 
@@ -102,7 +197,7 @@ def TV_E(circuitName, seedInitial):
     p=""
     i=1
 
-    while i!= loopTime:
+    while i != loopTime:
         for bit in reversedSeedInitial:
             lst.append(bit)
 
@@ -110,8 +205,11 @@ def TV_E(circuitName, seedInitial):
 
 
         Shiftedlst = [lst[-1]] + lst[:-1]
+
         print("Shifted: " + str(Shiftedlst))
         #for bit in lst:
+        #for i in Shiftedlst:
+
         if lst[len(seedInitial)-1]=='0' and Shiftedlst[2]=='0':
             Shiftedlst[2]='0'
 
@@ -157,7 +255,7 @@ def TV_E(circuitName, seedInitial):
 
         i = i+1
     p = p.join(tvSet)
-    p=p[0:n]
+    #p=p[0:n]
     reversedP = ''.join(reversed(p))
     outputFile.write(reversedP+"\n")
     b=len(p)-1
@@ -165,41 +263,51 @@ def TV_E(circuitName, seedInitial):
         for bit in p:
             lst.append(bit)
         Shiftedlst = [lst[-1]] + lst[:-1]
-        if lst[b] == '0' and Shiftedlst[2] == '0':
-            Shiftedlst[2] = '0'
+       # print(Shiftedlst)
+        y= sublist(lst,8)
+        x = sublist(Shiftedlst, 8)
 
-        elif lst[b] == '1' and Shiftedlst[2] == '0':
-            Shiftedlst[2] = '1'
+        k=0
 
-        elif lst[b] == '0' and Shiftedlst[2] == '1':
-            Shiftedlst[2] = '1'
+        while k<=len(y)-1 and k<=len(x)-1:
+            if y[k][7] == '0' and x[k][2] == '0':
+                x[k][2] = '0'
 
-        elif lst[b] == '1' and Shiftedlst[2] == '1':
-            Shiftedlst[2] = '0'
+            elif y[k][7] == '1' and x[k][2] == '0':
+                x[k][2] = '1'
 
-        if lst[b] == '0' and Shiftedlst[3] == '0':
-            Shiftedlst[3] = '0'
+            elif y[k][7] == '0' and x[k][2] == '1':
+                x[k][2] = '1'
 
-        elif lst[b] == '1' and Shiftedlst[3] == '0':
-            Shiftedlst[3] = '1'
+            elif y[k][7] == '1' and x[k][2] == '1':
+                x[k][2] = '0'
 
-        elif lst[b] == '0' and Shiftedlst[3] == '1':
-            Shiftedlst[3] = '1'
+            if y[k][7] == '0' and x[k][3] == '0':
+                x[k][3]='0'
 
-        elif lst[b] == '1' and Shiftedlst[3] == '1':
-            Shiftedlst[3] = '0'
+            elif y[k][7]== '1' and x[k][3] == '0':
+                x[k][3]= '1'
 
-        if lst[b] == '0' and Shiftedlst[4] == '0':
-            Shiftedlst[4] = '0'
-        elif lst[b] == '1' and Shiftedlst[4] == '0':
-            Shiftedlst[4] = '1'
+            elif y[k][7] == '0' and x[k][3] == '1':
+                x[k][3]= '1'
 
-        elif lst[b] == '0' and Shiftedlst[4] == '1':
-            Shiftedlst[4] = '1'
+            elif y[k][7] == '1' and x[k][3] == '1':
+                x[k][3]='0'
 
-        elif lst[b] == '1' and Shiftedlst[4] == '1':
-            Shiftedlst[4] = '0'
-        str1 = str1.join(Shiftedlst)
+            if y[k][7]== '0' and x[k][4] == '0':
+                x[k][4]='0'
+            elif y[k][7] == '1' and x[k][4] == '0':
+                x[k][4]='1'
+
+            elif y[k][7] == '0' and x[k][4] == '1':
+                x[k][4]='1'
+
+            elif y[k][7] == '1' and x[k][4] == '1':
+               x[k][4] = '0'
+            k=k+1
+        l=reduce(lambda f, g: f + g, x)
+        str1 = str1.join(l)
+        print(str1)
         toGo=''.join(reversed(str1))
         outputFile.write(toGo+"\n")
         p=str1
@@ -259,11 +367,11 @@ def main():
 
 
 
-    TV_A(cktFile, seedInitial)
+    #TV_A(cktFile, seedInitial)
     TV_B(cktFile, seedInitial)
     TV_C(cktFile, seedInitial)
     TV_D(cktFile, seedInitial)
-    TV_E(cktFile, seedInitial)
+    #TV_E(cktFile, seedInitial)
 
 if __name__ == "__main__":
     main()
